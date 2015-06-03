2b00ef2c0ed04b2646a97a698101423e6aec4faf
Jairo Serrano <jairo.serrano@gmail.com>
2015-06-03 15:36:22 -0500

    Ajuste al middleware por defecto

--- server5/app/Http/Middleware/Authenticate.php
+++ server5/app/Http/Middleware/Authenticate.php
@@ -32,18 +32,6 @@ class Authenticate {
 	 */
 	public function handle($request, Closure $next)
 	{
-		if ($this->auth->guest())
-		{
-			if ($request->ajax())
-			{
-				return response('Unauthorized.', 401);
-			}
-			else
-			{
-				return redirect()->guest('auth/login');
-			}
-		}
-
 		return $next($request);
 	}
 
